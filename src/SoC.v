3492547db9a13e578a80d8794a3af4e165c9d24f
Thomas Singer <tmssngr@users.noreply.github.com>
2024-02-22 21:18:30 +0100

    ld swapped commands

--- src/SoC.v
+++ src/SoC.v
@@ -858,7 +858,7 @@ module Processor(
                     $display("    ld @%h, %h", third, second);
                     expectedCycles <= 10;
 `endif
-                    aluA <= readRegister8(r8(second));
+                    register <= readRegister8(r8(third));
                     opType <= OP_LD;
                     canFetch <= 0;
                 end
@@ -1165,7 +1165,7 @@ module Processor(
                         register <= r8(third);
                     end
                     4'hF: begin
-                        register <= readRegister8(r8(third));
+                        aluA <= readRegister8(r8(second));
                     end
                     endcase
                 end
