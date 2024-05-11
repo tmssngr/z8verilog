4d7131ec188a2944fd6253bca8c46ec47558eef2
Thomas Singer <tmssngr@users.noreply.github.com>
2024-05-11 11:07:07 +0200

    use SIO for loading/saving

--- src/SoC_es40.v
+++ src/SoC_es40.v
@@ -183,6 +183,8 @@ module SoC_es40(
         .memDataWrite(memDataWrite),
         .memWrite(memWrite),
         .memStrobe(memStrobe),
+        .serialIn(serialIn),
+        .serialOut(serialOut),
         .isIsr(isIsr),
         .port2Out(port2),
         .port3Out(port3)
