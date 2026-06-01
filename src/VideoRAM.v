846ec677c91ce979eea2800793154882d0103e16
Thomas Singer <tmssngr@users.noreply.github.com>
2026-06-01 19:12:36 +0200

    JTC-6k (ES4.0)

--- src/VideoRAM.v
+++ src/VideoRAM.v
@@ -3,57 +3,50 @@
 `include "Memory.v"
 
 module VideoRAM(
-    input wire      clk, // assuming 8MHz
-    input wire[8:0] x,
-    input wire[7:0] y,
-    input wire      visible,
-    output wire     pixel,
-    output wire     valid
+    input wire       clk,
+    input wire[12:0] cAddr,
+    input wire[7:0]  cDataIn,
+    input wire       cStrobe,
+    input wire       cWrite,
+    output wire[7:0] cDataOut,
+
+    input wire[8:0]  x,
+    input wire[7:0]  y,
+    input wire       visible,
+    output wire      pixel,
+    output wire      valid
 );
-    reg [7:0] memory[0 : 2047];
-    reg [7:0] dataOut;
-    reg[10:0] addr;
-    wire strobe;
 
-    integer i;
-    initial begin
-        for (i = 0; i < 2048; i = i + 1) begin
-            memory[i] = 8'h0;
-        end
-        memory[11'h000] = 8'b00010000;
-        memory[11'h028] = 8'b00111000;
-        memory[11'h050] = 8'b01101100;
-        memory[11'h080] = 8'b11000110;
-        memory[11'h0A8] = 8'b11111110;
-        memory[11'h0D0] = 8'b11000110;
-        memory[11'h100] = 8'b11000110;
-        memory[11'h128] = 8'h0;
+    reg[12:0] vAddr = 0;
+    wire[7:0] vData;
+    wire      vStrobe;
 
-        memory[11'h027] = 8'b00000000;
-        memory[11'h04F] = 8'b01111000;
-        memory[11'h077] = 8'b00001100;
-        memory[11'h0A7] = 8'b01111100;
-        memory[11'h0CF] = 8'b11001100;
-        memory[11'h0F7] = 8'b11001100;
-        memory[11'h127] = 8'b01110110;
-        memory[11'h14F] = 8'b0;
-    end
+    wire[12:0] rAddr;
+    wire       rStrobe;
+    wire[12:0] ramAddr;
+    wire[7:0]  ramDataOut;
+    RAM8k ram(
+        .clk(clk),
+        .addr(ramAddr),
+        .dataIn(cDataIn),
+        .dataOut(cDataOut),
+        .write(cWrite),
+        .strobe(cStrobe)
+    );
 
-    always @(posedge clk) begin
-        if (strobe) begin
-            dataOut <= memory[addr];
-        end
-    end
+    assign ramAddr = cStrobe ? cAddr : vAddr;
+    assign vData = cDataOut;
 
     wire[5:0] xByteCounter = x[8:3];
     wire[2:0] xBitCounter = x[2:0];
     reg[1:0]  offsetCounter = 0;
-    wire[3:0] addrOffset = xByteCounter == 39 && offsetCounter[1] ? 4'd9 : 4'd1;
+    wire[3:0] vAddrOffset = xByteCounter == 39 && offsetCounter[1] ? 4'd9 : 4'd1;
 
     reg[7:0] dataSR = 0;
     reg[1:0] validSR = 0;
 
-    assign strobe = visible & xBitCounter == 0;
+
+    assign vStrobe = visible & xBitCounter == 0;
 
     always @(posedge clk) begin
         dataSR <= {dataSR[6:0], 1'b0};
@@ -70,12 +63,12 @@ module VideoRAM(
             end
 
             if (xBitCounter == 1) begin
-                dataSR <= dataOut;
-                addr <= addr + addrOffset;
+                dataSR <= vData;
+                vAddr <= vAddr + vAddrOffset;
             end
         end
     end
 
-    assign pixel = dataSR[7];
+    assign pixel = ~dataSR[7];
     assign valid = validSR[1];
 endmodule
